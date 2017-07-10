7bfcace9470a4f4782b5f56a7b4635e28fd603ee
Omar Sagga <osagga@outlook.com>
2017-07-10 10:26:36 -0400

    UnitTesting

---
+++ TumbleBitSetup.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TumbleBitSetup.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TumbleBitSetup.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89680496-53ad-43fe-a855-1136af4cb4c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TumbleBitSetup.Tests/ProtocolTests.cs
@@ -0,0 +1,35 @@
+﻿using TumbleBit_Setup;
+using System;
+using TumbleBitSetup;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TumbleBit_Setup.Tests
+{
+    [TestClass()]
+    public class ProtocolTests
+    {
+        [TestMethod()]
+        public void genKeyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace TumbleBitSetup.Tests
+{
+    [TestClass]
+    public class ProtocolTests
+    {
+        [TestMethod]
+        public void CanProveAndVerify()
+        {
+            var alpha = 41;
+            var key = genKey(new BigInteger("65537"));
+            var privKey = (RsaPrivateCrtKeyParameters) key.Private;
+            var pubKey = (RsaKeyParameters) key.Public;
+            byte[][] signature = proving(privKey.P, privKey.Q, privKey.PublicExponent, alpha);
+            Assert.IsTrue(verifying(pubKey, signature, alpha));
+        }
+    }
+}

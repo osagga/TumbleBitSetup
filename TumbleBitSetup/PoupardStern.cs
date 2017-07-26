615f102abf38847a683f4a273f0bb551c666022d
Omar Sagga <osagga@outlook.com>
2017-07-26 09:39:37 -0400

    Fixed one of the checks on N (#16)

--- TumbleBitSetup/PoupardStern.cs
+++ TumbleBitSetup/PoupardStern.cs
@@ -53,11 +53,10 @@ namespace TumbleBitSetup
             // N - phi(N)
             BigInteger NsubPhi = Modulus.Subtract(phi);
 
-            // (N-phi)*2^k << N => N/{(N-phi)*2^k} > 2^10
-            // So we check for N/{(N-phi)*2^k} <= 2^10
-            var p11 = NsubPhi.Multiply(Two.Pow(k));
-            var p1 = Modulus.Divide(p11);
-            if (p1.CompareTo(Two.Pow(10)) <= 0)
+            // if 2^{|N|-1}/{(N-phi)*2^k} <= 2^k
+            var p11 = Two.Pow(k);
+            var p1 = lowerLimit.Divide(NsubPhi.Multiply(p11));
+            if (p1.CompareTo(p11) <= 0)
                 throw new ArgumentOutOfRangeException("Bad RSA modulus N");
 
             // Generate K
